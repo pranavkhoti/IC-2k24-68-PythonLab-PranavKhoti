a2b196320a835afc83ed99f33fe628f9f04d71cb
Pranav Khoti <pranavkhoti9c@gmail.com>
2026-08-17 11:11:11 +0530

    Create variable_practice.py

---
+++ Lab1/variable_practice.py
@@ -0,0 +1,11 @@
+# This program demonstrates variables and their data types.
+
+name = "Pranav"
+age = 20
+height = 5.8
+student = True
+
+print("Name:", name, "Type:", type(name))
+print("Age:", age, "Type:", type(age))
+print("Height:", height, "Type:", type(height))
+print("Student:", student, "Type:", type(student))
