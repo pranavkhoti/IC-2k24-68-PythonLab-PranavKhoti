7e1fc99b428f4e5962e1728d8d6e322906a3fbf2
Pranav Khoti <pranavkhoti9c@gmail.com>
2026-08-17 11:57:00 +0530

    Create SectionB.py

---
+++ Lab1/SectionB.py
@@ -0,0 +1,14 @@
+print(2 + 3 * 4 - 1)                  
+print(10 % 3 + 2 ** 2)             
+print((5 + 3) / 2 == 4)            
+print(type(5 / 2))                   
+print("Python"[1:4])                 
+print("Hi\tThere\nBye")           
+ 
+# 13
+# 5
+# True
+# <class 'float'>
+# yth
+# Hi	There
+# Bye
