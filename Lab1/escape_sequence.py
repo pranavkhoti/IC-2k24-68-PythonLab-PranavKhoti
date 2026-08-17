e03190216292aa5ea9dbaf4c0b565d45a852efda
Pranav Khoti <pranavkhoti9c@gmail.com>
2026-08-17 11:53:07 +0530

    Create escape_sequence.py

---
+++ Lab1/escape_sequence.py
@@ -0,0 +1,8 @@
+# This program displays a simple receipt using escape sequences.
+
+print("Item\t\tPrice")
+print("-----\t\t-----")
+print("Pen\t\t₹10")
+print("Notebook\t₹50")
+print("Pencil\t\t₹5")
+print("\nTotal\t\t₹65")
